2d6f788e6378118eaddd306953972c5c2a9f1cce
Teja Nerella <nerella2@illinois.edu>
2025-09-04 13:35:17 -0500

    submitting mp0 for CS409

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
 /* Your JS here. */
 console.log('Hello World!')
+card=document.getElementById('card');
+myName=document.getElementById('name');
+myImage=document.getElementById('image');
+desc=document.getElementById('desc');
+
+gray=document.getElementById('gray');
+red=document.getElementById('red');
+blue=document.getElementById('blue');
+
+gray.addEventListener("click",()=>colorChange("gray"))
+blue.addEventListener("click",()=>colorChange("DodgerBlue"))
+red.addEventListener("click",()=>colorChange("Crimson"))
+myImage.addEventListener("click",()=>desc.classList.add('hidden'));
+myName.addEventListener("click",()=>desc.classList.remove('hidden'));
+
+
+function colorChange(color){
+    console.log(color);
+    card.style.backgroundColor=color;
+}
